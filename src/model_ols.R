6927a39b149cebe60809def6e9fe0b294bb5cc62
Vladimir CERCHEZ <vladimir.cerchez@consort-group.com>
2023-07-18 22:23:10 +0200

    SRC: add rmse for model estimation

--- src/model_ols.R
+++ src/model_ols.R
@@ -1,5 +1,6 @@
 # data preprocessing
 source("./src/data_preprocessing.R")
+library(Metrics)
 
 # fit OLS
 model_FR <- lm(y_train[mask_train_FR, "TARGET"] ~ ., data = X_train[mask_train_FR, ])
@@ -14,6 +15,14 @@ y_hat_train[!mask_train_FR] <- predict(model_DE, X_train[!mask_train_FR, ])
 
 y_hat_test[mask_test_FR] <- predict(model_FR, X_test[mask_test_FR, ])
 y_hat_test[!mask_test_FR] <- predict(model_DE, X_test[!mask_test_FR, ])
+
+# RMSE
+rmse_train_FR <- rmse(y_train[mask_train_FR, "TARGET"], y_hat_train[mask_train_FR]) %>% round(., 3)
+rmse_train_DE <- rmse(y_train[!mask_train_FR, "TARGET"], y_hat_train[!mask_train_FR]) %>% round(., 3)
+rmse_train <- rmse(y_train$TARGET, y_hat_train) %>% round(., 3)
+print(paste("RMSE on training set for FR: ", rmse_train_FR))
+print(paste("RMSE on training set for DE: ", rmse_train_DE))
+print(paste("RMSE on training set: ", rmse_train))
 
 # Spearman correlation for the predictions on the training set
 cor_train_FR <- cor(y_train[mask_train_FR, "TARGET"], y_hat_train[mask_train_FR], method = "spearman") %>% round(., 3)
--- src/model_regularized_ols.R
+++ src/model_regularized_ols.R
@@ -3,6 +3,7 @@ source("./src/data_preprocessing.R")
 
 library(glmnet)
 library(caret)
+library(Metrics)
 
 set.seed(2023)
 
@@ -48,6 +49,14 @@ y_hat_train[!mask_train_FR] <- predict(model_DE, X_train[!mask_train_FR, ])
 
 y_hat_test[mask_test_FR] <- predict(model_FR, X_test[mask_test_FR, ])
 y_hat_test[!mask_test_FR] <- predict(model_DE, X_test[!mask_test_FR, ])
+
+# RMSE
+rmse_train_FR <- rmse(y_train[mask_train_FR, "TARGET"], y_hat_train[mask_train_FR]) %>% round(., 3)
+rmse_train_DE <- rmse(y_train[!mask_train_FR, "TARGET"], y_hat_train[!mask_train_FR]) %>% round(., 3)
+rmse_train <- rmse(y_train$TARGET, y_hat_train) %>% round(., 3)
+print(paste("RMSE on training set for FR: ", rmse_train_FR))
+print(paste("RMSE on training set for DE: ", rmse_train_DE))
+print(paste("RMSE on training set: ", rmse_train))
 
 # Spearman correlation for the predictions on the training set
 cor_train_FR <- cor(y_train[mask_train_FR, "TARGET"], y_hat_train[mask_train_FR], method = "spearman") %>% round(., 3)
