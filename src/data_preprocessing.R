7a870220251650ca6e686ffbebd3afa6fb928f97
Vladimir CERCHEZ <vladimir.cerchez@consort-group.com>
2023-07-24 15:23:47 +0200

    SRC: add data standartization in data preparation
    It appeared that the data was actually not standartized: means != 0 and
    std != 1. Probably a random noise was added at the moment of
    standartization to anonymize the data. As a result, imputing missing
    values with 0 was somewhat arbitrary.
    
    Now the data is standartized before imputation.

--- src/data_preprocessing.R
+++ src/data_preprocessing.R
@@ -10,12 +10,19 @@ y_train <- read.csv("./data/y_train.csv")
 mask_train_FR <- X_train_raw$COUNTRY == "FR"
 mask_test_FR <- X_test_raw$COUNTRY == "FR"
 
-# Fill-in NAs
-X_train <- X_train_raw %>% replace(is.na(.), 0)
-X_test <- X_test_raw %>% replace(is.na(.), 0)
-
 # Drop unused cols
 columns_to_drop <- c("ID", "DAY_ID", "COUNTRY", "DE_FR_EXCHANGE", "FR_NET_IMPORT", "DE_NET_IMPORT")
 
-X_train <- X_train %>% select(!one_of(columns_to_drop))
-X_test <- X_test %>% select(!one_of(columns_to_drop))
+X_train <- X_train_raw %>% select(!one_of(columns_to_drop))
+X_test <- X_test_raw %>% select(!one_of(columns_to_drop))
+
+# Standartize vars
+mean_ <- sapply(X_train, mean, na.rm = TRUE)
+sd_ <- sapply(X_train, sd, na.rm = TRUE)
+
+X_train <- as.data.frame(scale(X_train, center = mean_, scale = sd_))
+X_test <- as.data.frame(scale(X_test, center = mean_, scale = sd_))
+
+# Fill-in NAs
+X_train <- X_train %>% replace(is.na(.), 0)
+X_test <- X_test %>% replace(is.na(.), 0)
